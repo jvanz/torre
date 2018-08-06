e27bba0e37ab2dec71b565ee20ab7636d6ff8e03
José Guilherme Vanz <guilherme.sft@gmail.com>
2018-08-06 01:25:20 -0300

    bio.py: authorize linkedin endpoint
    Adds two more endpoint to allow the user to authorized the app to read and
    show the LinkedIn data
    
    Signed-off-by: José Guilherme Vanz <guilherme.sft@gmail.com>

--- bio.py
+++ bio.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from os import environ
+from urlparse import parse_qs
+
+from flask import Flask, render_template, request
+from linkedin.linkedin import (LinkedInAuthentication, LinkedInApplication,
+        PERMISSIONS)
+
 from torre import get_bio
+
+cache = {}
 
 app = Flask(__name__)
 
@@ -12,3 +20,24 @@ def get_torrebio_html(personid):
     torrebio = get_bio(personid)
     print(torrebio)
     return render_template("torrebio.html", bio=torrebio)
+
+@app.route("/linkedin/authurl/<string:linkedinid>", methods=["GET"])
+def get_linkedin_authurl(linkedinid):
+    API_KEY = environ.get('LINKEDIN_API_KEY')
+    API_SECRET = environ.get('LINKEDIN_API_SECRET')
+    RETURN_URL = 'http://localhost:5000/linkedin/authorized'
+    authentication = LinkedInAuthentication(API_KEY, API_SECRET, RETURN_URL,
+        [PERMISSIONS.BASIC_PROFILE, PERMISSIONS.EMAIL_ADDRESS])
+    url = authentication.authorization_url
+    state = parse_qs(url)['state'][0]
+    cache[state] = authentication
+    return url
+
+@app.route("/linkedin/authorized", methods=["GET"])
+def get_linkedin_authorized():
+    code = request.args.get("code")
+    state = request.args.get("state")
+    cache[state].authorization_code = code
+    token = cache[state].get_access_token()
+    application = LinkedInApplication(token=token)
+    return render_template("index.html", linkedin_profile=application.get_profile())
